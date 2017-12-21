1ca18d10dc94be307064b38f6ce483f31b339ae1
soniab <sonia.bradley524@gmail.com>
2017-12-21 15:27:01 -0600

    added some styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,36 +5,36 @@ $('#start').on('click', function () {
 })
 
 var questions = [{
-    question: "During what Year was the movie 'Gone with the Wind' Released?",
-    answers: ["1980 ", "1942 ", "1963 ", "1940 ", "1975 "],
-    correctAnswer: "1940 "
+    question: "Where was the movie 'Gone with the Wind', filmed?",
+    answers: ["New York", "Hollywood", "South Carolina", "Los Angeles", "Atlanta"],
+    correctAnswer: "Atalnta"
 }, {
-    question: "Which Actress was in the 1959 hit movie 'Some Like Hot?'",
+    question: "Which Actress was in the 1959 hit movie, Some Like Hot?",
     answers: ["Audrey Hepburn", "Katharine Hepburn", "Vivien Leigh", "Marilyn Monroe", "Ann Rutherford"],
-    correctAnswer: "Marilyn Monroe "
+    correctAnswer: "Marilyn Monroe"
 }, {
     question: "During what year was Coco Chanel was born?",
-    answers: ["1883 ", "1983 ", "1953 ", "1963 ", "1923 "],
-    correctAnswer: "1883 "
+    answers: ["1883", "1983", "1953", "1963", "1923"],
+    correctAnswer: "1883"
 }, {
     question: "Who famously said 'Girls can do everything men can do...just backwards and in heels.'",
-    answers: ["Eva Gardner ", "Ginger Rogers ", "Elizabeth Taylor ", "Madonna ", "Fred Astaire "],
-    correctAnswer: "Ginger Rogers "
+    answers: ["Eva Gardner", "Ginger Rogers", "Elizabeth Taylor", "Madonna", "Fred Astaire"],
+    correctAnswer: "Ginger Rogers"
 }, {
     question: "What is Freddie Mercury?",
-    answers: ["car ", "planet ", "metal ", "singer ", "rock "],
-    correctAnswer: "singer "
+    answers: ["car", "planet", "metal", "singer", "rock"],
+    correctAnswer: "singer"
 }, {
-    question: "How old would Kurt Cobain be today (2017)?",
-    answers: ["50 ", "48 ", "51 ", "65 ", "55 "],
-    correctAnswer: "50 "
+    question: "How old would Kurt Cobain be today, 2017?",
+    answers: ["50", "48", "51", "65", "55"],
+    correctAnswer: "50"
 }];
 
 var game = {
     correct: 0,
     incorrect: 0,
     // seconds
-    counter: 10,
+    counter: 30,
     countdown: function () {
         // created counter function that will 'start' below
         game.counter--;
@@ -48,7 +48,7 @@ var game = {
     start: function () {
         // insert time, 1000seconds
         timer = setInterval(game.countdown,1000);
-        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>');
+        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">30</span> Seconds</h2>');
         $('#start').remove();
         for (var i = 0; i < questions.length; i++) {
             // append to subwrapper
@@ -107,7 +107,7 @@ var game = {
     result: function(){
         clearInterval(timer);
         $('#subwrapper h2').remove();
-        $('#subwrapper').html("<h2>All done!</h2");
+        $('#subwrapper').html("<h2>All done!</h2>");
         // print all correct and incorrect answers
         $('#subwrapper').append("<h3>Correct Answers: "+this.correct+"</h3>");
         $('#subwrapper').append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
